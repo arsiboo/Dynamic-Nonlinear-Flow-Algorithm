2df47be3362e590b3ad455ba74b9961de009671b
arsiboo <arsineh.boodaghian@gmail.com>
2023-08-02 19:05:12 +0200

    Update dynamic_nonlinear_flow.py

--- dynamic_nonlinear_flow.py
+++ dynamic_nonlinear_flow.py
@@ -1,6 +1,5 @@
 # modified dinitz flow algorithm Description:
 # This is a dynamic non-linear flow algorithm based on dinitz flow algorithm.
-# The algorithm has been modified for continous/discrete flow.
 # Furthermore, the algorithm is modified to provide two type of outputs: 1) Residual graph, 2) minmax graph.
 # The residual graph's lowest value indicates a strong bottleneck, the mixmax graph shows the persistency of the botttlneck.
 # If the variance in minmax graph is small, it means that there is a persistent bottleneck toward that ward.
